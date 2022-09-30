0291378c14e366580ff5f1052ed636b47a4f37f7
Mekhriddin Jumaev <jumaevmekhriddin10@gmail.com>
2022-09-30 16:48:01 +0500

    Initial Commit

---
+++ Animals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animals
+//
+//  Created by Mekhriddin Jumaev on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
